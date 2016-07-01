f244c847fe0922671e5d503aca23f30966d14f1f
switch <q547550831@outlook.com>
2016-07-01 19:57:54 +0800

    Fixed assertion error: Saved the parameter nextTurnNumber of method MockTurnNumberSequence.arrangeNextTurnNumber() into the field nextTurnNumber of class MockTurnNumberSequence.

--- td/src/main/java/kata/td/MockTurnNumberSequence.java
+++ td/src/main/java/kata/td/MockTurnNumberSequence.java
@@ -4,8 +4,10 @@ package kata.td;
  * Created by switch on 16/7/1.
  */
 public class MockTurnNumberSequence extends TurnNumberSequence {
-    public void arrangeNextTurnNumber(int nextTurnNumber) {
+    private int nextTurnNumber;
 
+    public void arrangeNextTurnNumber(int nextTurnNumber) {
+        this.nextTurnNumber = nextTurnNumber;
     }
 
     public void verifyMethodGetNextTurnNumberCalledOnce() {
