dd90668645efe9bb2ae3109a99ba7aa66e1b4fce
switch <q547550831@outlook.com>
2016-07-01 19:49:08 +0800

    Fixed compiler error: Made class MockTurnNumberSequence extend class TurnNumberSequence.

--- td/src/main/java/kata/td/MockTurnNumberSequence.java
+++ td/src/main/java/kata/td/MockTurnNumberSequence.java
@@ -3,7 +3,7 @@ package kata.td;
 /**
  * Created by switch on 16/7/1.
  */
-public class MockTurnNumberSequence {
+public class MockTurnNumberSequence extends TurnNumberSequence {
     public void arrangeNextTurnNumber(int nextTurnNumber) {
 
     }
