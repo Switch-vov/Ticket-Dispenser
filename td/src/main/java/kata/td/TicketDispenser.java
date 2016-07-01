23022a4f98dc8d804c5f0cf8bb5559cd31737b33
switch <q547550831@outlook.com>
2016-07-01 20:03:49 +0800

    Fixing assertion error: Finished TODO: Depending on a static method violate the Dependency Inversion Principle and Open-Closed Principle.

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -13,8 +13,7 @@ public class TicketDispenser {
 
     public TurnTicket getTurnTicket()
     {
-        // TODO: Depending on a static method violate the Dependency Inversion Principle and Open-Closed Principle.
-        int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
+        int newTurnNumber = turnNumberSequence.getNextTurnNumber();
         TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
         return newTurnTicket;
