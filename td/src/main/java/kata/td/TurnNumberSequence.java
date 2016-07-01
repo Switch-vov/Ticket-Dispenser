f1209151a0d98f89f6d6ace551f8025451eea352
switch <q547550831@outlook.com>
2016-07-01 20:36:47 +0800

    Renamed field _turnNumber of class TurnNumberSequence to be turnNumber.

--- td/src/main/java/kata/td/TurnNumberSequence.java
+++ td/src/main/java/kata/td/TurnNumberSequence.java
@@ -1,18 +1,18 @@
 package kata.td;
 
 public class TurnNumberSequence {
-    private static int _turnNumber = 0;
+    private static int turnNumber = 0;
 
     public TurnNumberSequence() {
         this(0);
     }
 
     public TurnNumberSequence(int firstNumber) {
-        this._turnNumber = firstNumber;
+        this.turnNumber = firstNumber;
     }
 
     public int getNextTurnNumber()
     {
-        return _turnNumber++;
+        return turnNumber++;
     }
 }
