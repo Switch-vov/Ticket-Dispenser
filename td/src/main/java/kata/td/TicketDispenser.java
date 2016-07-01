f261aaf9b4c7b654531b58f9910f74cc99b1056a
switch <q547550831@outlook.com>
2016-07-01 17:56:02 +0800

    Added TODO: Depending on a static method violate the Dependency Inversion Principle and Open-Closed Principle.

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -3,6 +3,7 @@ package kata.td;
 public class TicketDispenser {
     public TurnTicket getTurnTicket()
     {
+        // TODO: Depending on a static method violate the Dependency Inversion Principle and Open-Closed Principle.
         int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
         TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
