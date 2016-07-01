90f601c4312bffd4ac8cdf8f9f21260bfc1579b8
switch <q547550831@outlook.com>
2016-07-01 18:41:31 +0800

    Working on TODO: a new ticket should have the turn number subsequent to the previous ticket from another dispenser. Wrote an assertion for its test.

--- td/src/test/java/kata/td/TicketDispenserTest.java
+++ td/src/test/java/kata/td/TicketDispenserTest.java
@@ -26,7 +26,13 @@ public class TicketDispenserTest {
         assertEquals(1, newTicket.getTurnNumber() - previousTicket.getTurnNumber());
     }
 
-    // TODO-user-intent-test: a new ticket should have the turn number subsequent to the previous ticket from another dispenser
+    // TODO-user-intent-test-working-on: a new ticket should have the turn number subsequent to the previous ticket from another dispenser
+    @Test
+    public void a_new_ticket_should_hava_the_turn_number_subsequent_to_the_previous_ticket_from_another_dispenser() {
+
+        // Assert
+        assertEquals(1, newTicket.getTurnNumber() - previousTicket.getTurnNumber());
+    }
 
     // TODO-unit-test: the ticket dispenser should dispenser the ticket number 11 if give a turn number 11 to it
 
