e8d77871006cdb7bfb61c1351c1c84733b08f587
switch <q547550831@outlook.com>
2016-07-01 18:19:28 +0800

    Added an unit test TODO for class TicketDispenser.

--- td/src/test/java/kata/td/TicketDispenserTest.java
+++ td/src/test/java/kata/td/TicketDispenserTest.java
@@ -13,6 +13,9 @@ public class TicketDispenserTest {
     // TODO-user-intent-test: a new ticket should have the turn number subsequent to the previous ticket
     // TODO-user-intent-test: a new ticket should have the turn number subsequent to the previous ticket from another dispenser
 
+    // TODO-unit-test: the ticket dispenser should dispenser the ticket number 11 if give a turn number 11 to it
+
+
     // TODO-new-feature: the turn number sequence of the vip customers starts from 1001
     // TODO-new-feature: the turn number sequence of the regular customers starts from 2001
 }
