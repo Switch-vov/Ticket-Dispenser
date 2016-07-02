2cacff19e3916de69929f50f9fa41f96e0d2cdbb
switch <q547550831@outlook.com>
2016-07-02 11:43:44 +0800

    Fixed compiler error: Created constructor TicketDispenser(TurnNumberSequence).

--- td/src/main/java/kata/td/TicketDispenser.java
+++ td/src/main/java/kata/td/TicketDispenser.java
@@ -1,6 +1,10 @@
 package kata.td;
 
 public class TicketDispenser {
+    public TicketDispenser(TurnNumberSequence turnNumberSequence) {
+
+    }
+
     public TurnTicket getTurnTicket()
     {
         // TODO: Depending on a static method violate the Dependency Inversion Principle and Open-Closed Principle.
